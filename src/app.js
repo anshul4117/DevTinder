303b1d866d4bc3674bbeeb7025737b26667cbf19
anshul4117 <anshul41171@gmail.com>
2024-11-30 23:17:12 +0530

    some changes

--- src/app.js
+++ src/app.js
@@ -11,11 +11,11 @@ app.use(cookieParser());
 
 const authRouter = require('./routes/auth.js');
 const profileRouter = require('./routes/profile.js');
-const conncetionRouter = require('./routes/profile.js');
+const requestRouter = require('./routes/request.js');
 
 app.use('/', authRouter)
 app.use('/', profileRouter)
-app.use('/', conncetionRouter   )
+app.use('/', requestRouter)
 
 
 
