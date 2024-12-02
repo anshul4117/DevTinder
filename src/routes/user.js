694c86a56bbf83b6bea23e62de28354184125764
anshul4117 <anshul41171@gmail.com>
2024-12-02 15:49:28 +0530

    "Updated user routes: added User model, modified connection request and feed logic, and implemented pagination"

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const userRouter = express.Router();
 const validator = require('validator');
+const User = require('../models/user')
 const { userAuth } = require('../middleware/auth');
-const ConnectionRequest = require('../models/conncetionRequest')
+const ConnectionRequest = require('../models/conncetionRequest');
+const { set } = require('mongoose');
+const USER_SAFE_DATA = "firstName lastName age gender about skills";
 
 userRouter.get('/user/requests', userAuth, async (req, res) => {
     try {
@@ -22,7 +25,6 @@ userRouter.get('/user/requests', userAuth, async (req, res) => {
         })
     }
 });
-
 
 userRouter.get('/user/connections', userAuth, async (req, res) => {
     try {
@@ -58,11 +60,29 @@ userRouter.get('/user/feed', userAuth, async (req, res) => {
     try {
 
         const loggedInUser = req.user;
-        const feed = await ConnectionRequest.find({
-            $nor: [
-                { fromUserId: loggedInUser._id },
-            ]
-        })
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 50 ? 50 : limit;
+        const skip = (page - 1) * limit;
+
+        const connectionRequest = await ConnectionRequest.find({
+            $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }]
+        }).select("fromUserId toUserId");
+
+        const hideUsersFromFeed = new Set();
+        connectionRequest.forEach(req => {
+            hideUsersFromFeed.add(req.fromUserId);
+            hideUsersFromFeed.add(req.toUserId);
+        });
+
+        const users = await User.find({
+            _id: { $ne: loggedInUser._id },
+            _id: { $nin: Array.from(hideUsersFromFeed) },
+        }).select(USER_SAFE_DATA)
+            .skip(skip)
+            .limit(limit);
+
+        res.send(users)
 
     } catch (error) {
         res.status(400).json({
